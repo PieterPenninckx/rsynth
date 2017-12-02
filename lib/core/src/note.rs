4303a076df9b9c4c32d82237f3fae227b56bea1a
Alexander L <Alexander L>
2017-12-02 14:56:02 -0500

    removed overcomplicated builder in notedata

--- lib/core/src/note.rs
+++ lib/core/src/note.rs
@@ -1,3 +1,15 @@
+
+/// 11110000
+const STATUS_MASK: u8 = 0xF0;
+/// 00001111
+const CHANNEL_MASK: u8 = 0x0F;
+/// Default note is an A4
+const DEFAULT_NOTE: u8 = 69u8;
+/// Default velocity is full at 127
+const DEFAULT_VELOCITY: u8 = 127u8;
+/// The default channel is 0, which is usually what we're targeting.
+const DEFAULT_CHANNEL: u8 = 0u8;
+
 /// Contains all data needed to play a note
 #[derive(Clone)]
 pub struct NoteData {
@@ -14,46 +26,12 @@ pub struct NoteData {
 /// Return a default `NoteData` object, with full velocity and a note of middle C.
 impl Default for NoteData {
     fn default() -> NoteData {
-        NoteData { note: 60u8, velocity: 127u8, state: NoteState::Nil, channel: 0 }
+        NoteData { note: DEFAULT_NOTE, velocity: DEFAULT_VELOCITY, state: NoteState::Nil, channel: DEFAULT_CHANNEL }
     }
 }
 
-/// Builder methods for `NoteData`
-impl NoteData {
-    /// Return a new default `NoteData` structure
-    pub fn new() -> Self {
-        NoteData::default()
-    }
-
-    /// Modify the note and return a mutable reference to self
-    pub fn note(&mut self, note: u8) -> &mut Self {
-        self.note = note;
-        self
-    }
-
-    /// Modify the velocity and return a mutable reference to self
-    pub fn velocity(&mut self, velocity: u8) -> &mut Self {
-        self.velocity = velocity;
-        self
-    }
-
-    /// Modify the channel and return a mutable reference to self
-    pub fn channel(&mut self, channel: u8) -> &mut Self {
-        self.channel = channel;
-        self
-    }
-
-    /// Modify the state and return a mutable reference to self
-    pub fn state(&mut self, state: NoteState) -> &mut Self {
-        self.state = state;
-        self
-    }
-
-    /// Finalize all data and return a new `NoteData` structure
-    pub fn finalize(self) -> Self {
-        NoteData { note: self.note, velocity: self.velocity, channel: self.channel, state: self.state }
-    }
-
+/// This contains all data that can be constructed from a MIDI note signal.
+impl NoteData { 
     /// Convert note data obtained from the host into a `NoteData` structure.
     pub fn data(data: [u8; 3]) -> NoteData {
 		let (state, channel) = NoteState::state_and_channel(data[0]);
@@ -64,11 +42,6 @@ impl NoteData {
 			channel: channel }
 	}
 }
-
-/// 11110000
-const STATUS_MASK: u8 = 0xF0;
-/// 00001111
-const CHANNEL_MASK: u8 = 0x0F;
 
 /// A more readable boolean for keeping track of a note's state
 #[derive(PartialEq, Clone)]
