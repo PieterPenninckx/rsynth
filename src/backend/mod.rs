af94d3557d9a7c5abeab70614986ed12c1a57adb
Pieter Penninckx <Pieter.Penninckx@scarlet.be>
2018-09-19 19:40:24 +0200

    Continue on putting jack and vst behind a common trait.

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,45 +1,42 @@
 use vst::buffer::{Inputs, Outputs};
-#[cfg(feature="jack-backend")]
-use jack::AudioIn;
 
-pub trait InputAudioChannel<T> {
-    fn slice(&self) -> &[T];
+pub trait InputAudioChannel<'a, T> {
+    fn slice(&'a self) -> &'a [T];
 }
 
-pub trait OutputAudioChannel<T> {
-    fn slice(&mut self) -> &mut[T];
+pub trait OutputAudioChannel<'a, T> {
+    fn slice(&'a mut self) -> &'a mut[T];
 }
 
-impl<'a, T> InputAudioChannel<T> for &'a [T] {
-    fn slice(& self) -> & [T] {
+impl<'a, T> InputAudioChannel<'a, T> for &'a [T] {
+    fn slice(&'a self) -> &'a [T] {
         self
     }
 }
 
-
-impl<'a, T> OutputAudioChannel<T> for &'a mut[T] {
-    fn slice(& mut self) -> & mut [T] {
+impl<'a, T> OutputAudioChannel<'a, T> for &'a mut[T] {
+    fn slice(&'a mut self) -> &'a mut [T] {
         self
     }
 }
 
-pub trait InputAudioChannelGroup<C, T>: Sized
-    where C: InputAudioChannel<T>,
+pub trait InputAudioChannelGroup<'a, C, T>: Sized
+    where C: InputAudioChannel<'a, T>,
 {
     fn len(&self) -> usize;
     fn get(&self, index: usize) -> Option<C>;
     fn split_at(&self, index: usize) -> (Self, Self);
 }
 
-pub trait OutputAudioChannelGroup<C, T>: Sized
-    where C: OutputAudioChannel<T>,
+pub trait OutputAudioChannelGroup<'a, C, T>: Sized
+    where C: OutputAudioChannel<'a, T>,
 {
     fn len(&self) -> usize;
     fn get_mut(&mut self, index: usize) -> Option<C>;
     fn split_at_mut(&mut self, index: usize) -> (Self, Self);
 }
 
-impl<'a, T:'a> InputAudioChannelGroup<&'a [T], T> for Inputs<'a, T> {
+impl<'a, T:'a> InputAudioChannelGroup<'a, &'a [T], T> for Inputs<'a, T> {
     fn len(&self) -> usize {
         Inputs::len(self)
     }
@@ -57,7 +54,7 @@ impl<'a, T:'a> InputAudioChannelGroup<&'a [T], T> for Inputs<'a, T> {
     }
 }
 
-impl<'a, T: 'a> OutputAudioChannelGroup<&'a mut [T], T> for Outputs<'a, T> {
+impl<'a, T: 'a> OutputAudioChannelGroup<'a, &'a mut [T], T> for Outputs<'a, T> {
     fn len(&self) -> usize {
         Outputs::len(self)
     }
@@ -76,6 +73,41 @@ impl<'a, T: 'a> OutputAudioChannelGroup<&'a mut [T], T> for Outputs<'a, T> {
 }
 
 #[cfg(feature="jack-backend")]
-struct JackInputAudioChannelGroup{
-    channels: Vec<AudioIn>
+mod jack_backend {
+    use jack::{Port, AudioIn, AudioOut, ProcessScope};
+
+    use super::{
+        InputAudioChannel,
+        OutputAudioChannel,
+        InputAudioChannelGroup,
+        OutputAudioChannelGroup
+    };
+
+    struct AudioInWrapper<'ps> {
+        ps: &'ps ProcessScope,
+        audio_in: Port<AudioIn>
+    }
+
+    impl<'ps> InputAudioChannel<'ps, f32> for AudioInWrapper<'ps> {
+        fn slice(&'ps self) -> &'ps [f32]{
+            self.audio_in.as_slice(&self.ps)
+        }
+    }
+
+    struct AudioOutWrapper<'ps> {
+        ps: &'ps ProcessScope,
+        audio_out: Port<AudioOut>
+    }
+
+    impl<'ps> OutputAudioChannel<'ps, f32> for AudioOutWrapper<'ps> {
+        fn slice(&'ps mut self) -> &'ps mut [f32] {
+            self.audio_out.as_mut_slice(&self.ps)
+        }
+    }
+
+    struct JackInputAudioChannelGroup{
+        channels: Vec<AudioIn>
+    }
+
+
 }
