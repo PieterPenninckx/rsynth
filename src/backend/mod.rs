38af94f9120a6a0e0bd85042605173407cda8a84
Pieter Penninckx <Pieter.Penninckx@scarlet.be>
2018-09-22 14:24:12 +0200

    Add test to check that traits work.

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -28,12 +28,12 @@ pub trait InputAudioChannelGroup<'a, C, T>: Sized
     fn split_at(&self, index: usize) -> (Self, Self);
 }
 
-pub trait OutputAudioChannelGroup<'a, C, T>: Sized
+pub trait OutputAudioChannelGroup<'a, 'b, C, T>: Sized
     where C: OutputAudioChannel<'a, T>,
 {
     fn len(&self) -> usize;
-    fn get_mut(&mut self, index: usize) -> Option<C>;
-    fn split_at_mut(&mut self, index: usize) -> (Self, Self);
+    fn get_mut(&'b mut self, index: usize) -> Option<C>;
+    fn split_at_mut(&'a mut self, index: usize) -> (Self, Self);
 }
 
 impl<'a, T:'a> InputAudioChannelGroup<'a, &'a [T], T> for Inputs<'a, T> {
@@ -54,12 +54,12 @@ impl<'a, T:'a> InputAudioChannelGroup<'a, &'a [T], T> for Inputs<'a, T> {
     }
 }
 
-impl<'a, T: 'a> OutputAudioChannelGroup<'a, &'a mut [T], T> for Outputs<'a, T> {
+impl<'a, T: 'a> OutputAudioChannelGroup<'a, 'a, &'a mut [T], T> for Outputs<'a, T> {
     fn len(&self) -> usize {
         Outputs::len(self)
     }
 
-    fn get_mut(&mut self, index: usize) -> Option<&'a mut [T]> {
+    fn get_mut(&'a mut self, index: usize) -> Option<&'a mut [T]> {
         if index < self.len() {
             Some(Outputs::get_mut(self, index))
         } else {
@@ -74,7 +74,7 @@ impl<'a, T: 'a> OutputAudioChannelGroup<'a, &'a mut [T], T> for Outputs<'a, T> {
 
 #[cfg(feature="jack-backend")]
 mod jack_backend {
-    use jack::{Port, AudioIn, AudioOut, ProcessScope};
+    use jack::{Port, AudioIn, AudioOut, ProcessScope, Client, ClosureProcessHandler, ClientOptions, Control};
 
     use super::{
         InputAudioChannel,
@@ -88,7 +88,7 @@ mod jack_backend {
         audio_in: Port<AudioIn>
     }
 
-    impl<'ps> InputAudioChannel<'ps, f32> for AudioInWrapper<'ps> {
+    impl<'ps> InputAudioChannel<'ps, f32> for &'ps AudioInWrapper<'ps> {
         fn slice(&'ps self) -> &'ps [f32]{
             self.audio_in.as_slice(&self.ps)
         }
@@ -96,18 +96,90 @@ mod jack_backend {
 
     struct AudioOutWrapper<'ps> {
         ps: &'ps ProcessScope,
-        audio_out: Port<AudioOut>
+        audio_out: &'ps mut Port<AudioOut>
     }
 
-    impl<'ps> OutputAudioChannel<'ps, f32> for AudioOutWrapper<'ps> {
+    impl<'g, 'ps> OutputAudioChannel<'ps, f32> for &'g mut AudioOutWrapper<'ps> {
         fn slice(&'ps mut self) -> &'ps mut [f32] {
             self.audio_out.as_mut_slice(&self.ps)
         }
     }
 
-    struct JackInputAudioChannelGroup{
-        channels: Vec<AudioIn>
+    struct JackInputAudioChannelGroup<'ps> {
+        channels: &'ps [AudioInWrapper<'ps>]
     }
 
+    impl<'ps> InputAudioChannelGroup<'ps, &'ps AudioInWrapper<'ps>, f32> for JackInputAudioChannelGroup<'ps>
+    {
+        fn len(&self) -> usize {
+            self.channels.len()
+        }
 
+        fn get(&self, index: usize) -> Option<&'ps AudioInWrapper<'ps>> {
+            self.channels.get(index)
+        }
+
+        fn split_at(&self, index: usize) -> (Self, Self) {
+            let (first_channels, last_channels) = self.channels.split_at(index);
+            let first = JackInputAudioChannelGroup {
+                channels: first_channels
+            };
+            let last = JackInputAudioChannelGroup {
+                channels: last_channels
+            };
+            (first, last)
+        }
+    }
+
+    struct JackOutputAudioChannelGroup<'g, 'ps> where 'ps: 'g{
+        channels: &'g mut [AudioOutWrapper<'ps>]
+    }
+
+    impl<'g, 'ps> OutputAudioChannelGroup<'ps, 'g, &'g mut AudioOutWrapper<'ps>, f32> for JackOutputAudioChannelGroup<'g, 'ps> where 'ps: 'g{
+        fn len(&self) -> usize {
+            self.channels.len()
+        }
+
+        fn get_mut(&'g mut self, index: usize) -> Option<&'g mut AudioOutWrapper<'ps>> {
+            self.channels.get_mut(index)
+        }
+
+        fn split_at_mut(&'g mut self, index: usize) -> (Self, Self) {
+            let (first_channels, last_channels) = self.channels.split_at_mut(index);
+            let first = JackOutputAudioChannelGroup {
+                channels: first_channels
+            };
+            let last = JackOutputAudioChannelGroup {
+                channels: last_channels
+            };
+            (first, last)
+        }
+    }
+
+    // Is in fact a test.
+    fn test_synth() {
+        let (client, _status) =
+            Client::new("client", ClientOptions::NO_START_SERVER).unwrap();
+
+        let mut out_port = client
+            .register_port("out", AudioOut::default())
+            .unwrap();
+        let cback = move |_: &Client, ps: &ProcessScope| -> Control {
+            let out_wrapper = AudioOutWrapper{
+                ps: ps,
+                audio_out: &mut out_port
+            };
+            let mut wrapper_group = vec![out_wrapper];
+            let mut group = JackOutputAudioChannelGroup {
+                channels: &mut wrapper_group
+            };
+            let channel = group.get_mut(0).unwrap();
+            // TODO: Fix compile error when uncommenting the next line.
+//            let _slice : &mut[f32] = channel.slice();
+            Control::Continue
+        };
+        let _active_client = client
+            .activate_async((), ClosureProcessHandler::new(cback))
+            .unwrap();
+    }
 }
