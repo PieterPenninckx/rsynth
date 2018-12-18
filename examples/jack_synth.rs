6ddaeb50137dfdebda0586033ae8c557e6551f15
Pieter Penninckx <Pieter.Penninckx@scarlet.be>
2018-12-18 10:07:46 +0100

    Add documentation about running the jack synthesizer.

--- examples/jack_synth.rs
+++ examples/jack_synth.rs
@@ -1,3 +1,41 @@
+// An example of a software synthesizer using the JACK back-end.
+// The code that is shared between all backends is in the `test_synth.rs` file.
+//
+// Compiling
+// =========
+// You can compile this example with
+// ```
+// cargo build --release --examples --features jack-backend
+// ```
+// This generates a standalone application that you can
+// find under `target/release/examples/jack_synth`
+//
+// Running
+// =======
+// In order to run, you need three steps. Below, we discuss for each platform
+// (only Linux for now) how you can do this.
+//
+// 1. Start the `jack` daemon.
+// 2. Start the application generated during compiling.
+// 3. Connect the audio output of the synthesizer to the "system" audio input
+// 4. (Optionally) start a midi keyboard simulator
+// 5. Connect the midi keyboard output to the synthesizer midi input
+// 6. Start making some noise!
+//
+// ## Running under Linux
+// I think the easiest way is to use `qjackctl` and maybe `jack-keyboard`.
+// Then the steps become
+//
+// 1. Start `qjackctl`. Optionally configure some stuff. Click the "start" button.
+// 2. Start the synthesizer generated during compiling. This needs to be done after
+//    the jack daemon was started in step 1 because it automatically registers its ports upon
+//    startup.
+// 3. In Qjackctl, click on the "Connect" button. Under the "Audio" tab, connect the
+//    synthesizer to the system.
+// 4. (optionally) start `jack-keyboard`.
+// 5. In Qjackctl, in the "Connections" window, under the "Midi" tab, connect the
+//    midi keyboard to the synthesizer.
+// 6. Press keys on the midi keyboard.
 #[macro_use]
 extern crate log;
 extern crate simplelog;
