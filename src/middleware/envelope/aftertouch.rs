bd15feb60cfc41c768e53d502b347b3ad43ea955
Pieter Penninckx <pieter.penninckx@scarlet.be>
2019-07-14 18:03:33 +0200

    Continue work on aftertouch.

--- src/middleware/envelope/aftertouch.rs
+++ src/middleware/envelope/aftertouch.rs
@@ -1,19 +1,22 @@
+use super::EnvelopeContext;
 use crate::envelope::Envelope;
 use crate::event::{EventHandler, RawMidiEvent, Timed};
-pub struct AfterTouchMarker;
-use super::EnvelopeContext;
-use crate::dev_utilities::transparent::Transparent;
+use crate::{dev_utilities::transparent::Transparent, Plugin};
+use asprim::AsPrim;
 use core::marker::PhantomData;
+use num_traits::Float;
 #[cfg(feature = "stable")]
 use syllogism::{Distinction, Specialize};
 
-pub struct AfterTouchContext<E> {
-    envelope: E,
+pub struct AfterTouchContext<Envl> {
+    envelope: Envl,
 }
 
-impl<E> EnvelopeContext for AfterTouchContext<E> {
+pub struct AfterTouchMarker;
+
+impl<Envl> EnvelopeContext for AfterTouchContext<Envl> {
     type Marker = AfterTouchMarker;
-    type Data = E;
+    type Data = Envl;
     fn data(&mut self) -> &mut Self::Data {
         &mut self.envelope
     }
@@ -37,30 +40,21 @@ impl AfterTouchEvent for RawMidiEvent {
     }
 }
 
-impl<T> AfterTouchEvent for Timed<T>
+pub struct AfterTouchMiddleware<Event, Envl, Child, T>
 where
-    T: AfterTouchEvent,
+    for<'a> Envl: Envelope<'a, T>,
 {
-    fn aftertouch(&self) -> Option<u8> {
-        self.event.aftertouch()
-    }
-}
-
-pub struct AfterTouchMiddleware<Event, E, C, T>
-where
-    for<'a> E: Envelope<'a, T>,
-{
-    envelope: AfterTouchContext<E>,
-    child: C,
+    envelope_context: AfterTouchContext<Envl>,
+    child: Child,
     _phantom_event: PhantomData<Event>,
     _phantom_t: PhantomData<T>,
 }
 
-impl<Event, E, C, T> Transparent for AfterTouchMiddleware<Event, E, C, T>
+impl<Event, Envl, Child, T> Transparent for AfterTouchMiddleware<Event, Envl, Child, T>
 where
-    for<'a> E: Envelope<'a, T>,
+    for<'a> Envl: Envelope<'a, T>,
 {
-    type Inner = C;
+    type Inner = Child;
 
     fn get(&self) -> &Self::Inner {
         &self.child
@@ -71,13 +65,13 @@ where
     }
 }
 
-impl<Event: AfterTouchEvent, E, C, T> AfterTouchMiddleware<Event, E, C, T>
+impl<Event: AfterTouchEvent, Envl, Child, T> AfterTouchMiddleware<Event, Envl, Child, T>
 where
-    for<'a> E: Envelope<'a, T>,
+    for<'a> Envl: Envelope<'a, T>,
 {
-    fn new(child: C, envelope: E) -> Self {
+    fn new(child: Child, envelope: Envl) -> Self {
         Self {
-            envelope: AfterTouchContext { envelope },
+            envelope_context: AfterTouchContext { envelope },
             child,
             _phantom_event: PhantomData,
             _phantom_t: PhantomData,
@@ -85,14 +79,14 @@ where
     }
 }
 
-impl<Event, E, C, T> AfterTouchMiddleware<Event, E, C, T>
+impl<Event, Envl, Child, T> AfterTouchMiddleware<Event, Envl, Child, T>
 where
     Event: AfterTouchEvent,
-    for<'a> E: Envelope<'a, T, EventType = Timed<u8>>,
+    for<'a> Envl: Envelope<'a, T, EventType = Timed<u8>>,
 {
     fn handle_aftertouch_event(&mut self, event: Timed<Event>) {
         if let Some(aftertouch) = event.event.aftertouch() {
-            self.envelope.envelope.insert_event(Timed {
+            self.envelope_context.envelope.insert_event(Timed {
                 time_in_frames: event.time_in_frames,
                 event: aftertouch,
             })
@@ -101,31 +95,33 @@ where
 }
 
 #[cfg(not(feature = "stable"))]
-// TODO: "wrap" the context.
-impl<Event, E, C, T, GenericEvent, Context> EventHandler<GenericEvent, Context>
-    for AfterTouchMiddleware<Event, E, C, T>
+impl<Event, Envl, Child, T, GenericEvent, Context> EventHandler<GenericEvent, Context>
+    for AfterTouchMiddleware<Event, Envl, Child, T>
 where
     Event: AfterTouchEvent,
-    for<'a> E: Envelope<'a, T, EventType = Timed<u8>>,
-    C: EventHandler<GenericEvent, Context>,
+    for<'a> Envl: Envelope<'a, T, EventType = Timed<u8>>,
+    for<'ac, 'cc> Child:
+        EventHandler<GenericEvent, EnvelopeContextWrapper<'ac, 'cc, Context, Envl>>,
 {
     default fn handle_event(&mut self, event: GenericEvent, context: &mut Context) {
-        self.child.handle_event(event, context);
+        let mut wrapped_context = EnvelopeContextWrapper::new(&mut self.envelope_context, context);
+        self.child.handle_event(event, &mut wrapped_context);
     }
 }
 
 #[cfg(not(feature = "stable"))]
-// TODO: "wrap" the context.
-impl<Event, E, C, T, Context> EventHandler<Timed<Event>, Context>
-    for AfterTouchMiddleware<Event, E, C, T>
+impl<Event, Envl, Child, T, Context> EventHandler<Timed<Event>, Context>
+    for AfterTouchMiddleware<Event, Envl, Child, T>
 where
     Event: AfterTouchEvent,
-    for<'a> E: Envelope<'a, T, EventType = Timed<u8>>,
-    C: EventHandler<Timed<Event>, Context>,
+    for<'a> Envl: Envelope<'a, T, EventType = Timed<u8>>,
+    for<'ac, 'cc> Child:
+        EventHandler<Timed<Event>, EnvelopeContextWrapper<'ac, 'cc, Context, Envl>>,
 {
     fn handle_event(&mut self, event: Timed<Event>, context: &mut Context) {
         self.handle_aftertouch_event(event);
-        self.child.handle_event(event, context);
+        let mut wrapped_context = EnvelopeContextWrapper::new(&mut self.envelope_context, context);
+        self.child.handle_event(event, &mut wrapped_context);
     }
 }
 
@@ -143,13 +139,46 @@ where
         match event.specialize() {
             Distinction::Special(special) => {
                 self.handle_aftertouch_event(special);
-                let mut wrapped_context = EnvelopeContextWrapper::new(&mut self.envelope, context);
+                let mut wrapped_context =
+                    EnvelopeContextWrapper::new(&mut self.envelope_context, context);
                 self.child.handle_event(special, &mut wrapped_context);
             }
             Distinction::Generic(generic) => {
-                let mut wrapped_context = EnvelopeContextWrapper::new(&mut self.envelope, context);
+                let mut wrapped_context =
+                    EnvelopeContextWrapper::new(&mut self.envelope_context, context);
                 self.child.handle_event(generic, &mut wrapped_context);
             }
         }
+    }
+}
+
+impl<Event, Envl, Child, T, Context> Plugin<Context> for AfterTouchMiddleware<Event, Envl, Child, T>
+where
+    for<'a> Envl: Envelope<'a, T, EventType = Timed<u8>>,
+    for<'ac, 'cc> Child: Plugin<EnvelopeContextWrapper<'ac, 'cc, Context, Envl>>,
+{
+    const NAME: &'static str = Child::NAME;
+    const MAX_NUMBER_OF_AUDIO_INPUTS: usize = Child::MAX_NUMBER_OF_AUDIO_INPUTS;
+    const MAX_NUMBER_OF_AUDIO_OUTPUTS: usize = Child::MAX_NUMBER_OF_AUDIO_OUTPUTS;
+
+    fn audio_input_name(index: usize) -> String {
+        Child::audio_input_name(index)
+    }
+
+    fn audio_output_name(index: usize) -> String {
+        Child::audio_output_name(index)
+    }
+
+    fn set_sample_rate(&mut self, sample_rate: f64) {
+        self.child.set_sample_rate(sample_rate);
+    }
+
+    fn render_buffer<F>(&mut self, inputs: &[&[F]], outputs: &mut [&mut [F]], context: &mut Context)
+    where
+        F: Float + AsPrim,
+    {
+        let mut wrapped_context = EnvelopeContextWrapper::new(&mut self.envelope_context, context);
+        self.child
+            .render_buffer(inputs, outputs, &mut wrapped_context);
     }
 }
