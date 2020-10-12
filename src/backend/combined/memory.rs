f607036694b8db9a13f2d54e5baf73122a597abf
PieterPenninckx <pieter.penninckx@scarlet.be>
2020-10-12 16:19:10 +0200

    Add convenience conversions for using the `wav` crate. (#98)

--- src/backend/combined/memory.rs
+++ src/backend/combined/memory.rs
@@ -1,45 +1,82 @@
 //! In-memory backend, useful for testing.
 use super::{AudioReader, AudioWriter};
 use crate::buffer::{AudioBufferIn, AudioBufferOut, AudioChunk};
+#[cfg(feature = "backend-combined-wav")]
+use dasp_sample::{FromSample, I24};
+use std::borrow::Borrow;
+use std::marker::PhantomData;
+#[cfg(feature = "backend-combined-wav")]
+use wav::{BitDepth, Header};
 
 /// An [`AudioReader`] that reads from a given [`AudioChunk`].
 /// The generic parameter type `S` represents the sample type.
 ///
+#[cfg_attr(
+    feature = "backend-combined-wav",
+    doc = "\
+# Example
+
+The following example illustrates how an [`AudioChunk`] can be generated from a wav file.
+
+_Remark_ the example assumes the `rsynth` crate is compiled with the `backend-combined-wav`.
+
+_Remark_ the example does not use proper error handling.
+```
+extern crate wav;
+
+use std::fs::File;
+use std::path::Path;
+use rsynth::backend::combined::memory::AudioChunkReader;
+
+fn create_reader_from_file(filename: &str) {
+    let mut file = File::open(Path::new(filename)).unwrap();
+    let (header, samples) = wav::read(&mut file).unwrap();
+    let reader = AudioChunkReader::<f32, _>::from((header, samples));
+}
+```
+"
+)]
+///
 /// [`AudioReader`]: ../trait.AudioReader.html
 /// [`AudioChunk`]: ../../../buffer/struct.AudioChunk.html
-pub struct AudioBufferReader<'b, S>
+pub struct AudioChunkReader<S, T>
 where
+    T: Borrow<AudioChunk<S>>,
     S: Copy,
 {
     frames_per_second: u64,
     frame: usize,
-    buffer: &'b AudioChunk<S>,
+    chunk: T,
+    phantom: PhantomData<S>,
 }
 
-impl<'b, S> AudioBufferReader<'b, S>
+impl<S, T> AudioChunkReader<S, T>
 where
+    T: Borrow<AudioChunk<S>>,
     S: Copy,
 {
     /// Construct a new `AudioBufferReader` with the given [`AudioChunk`] and
     /// sample rate in frames per second.
     ///
     /// [`AudioChunk`]: ../../../buffer/struct.AudioChunk.html
-    pub fn new(buffer: &'b AudioChunk<S>, frames_per_second: u64) -> Self {
+    pub fn new(buffer: T, frames_per_second: u64) -> Self {
         Self {
-            buffer,
+            chunk: buffer,
             frames_per_second,
             frame: 0,
+            phantom: PhantomData::<S>,
         }
     }
 }
 
-impl<'b, S> AudioReader<S> for AudioBufferReader<'b, S>
+impl<S, T> AudioReader<S> for AudioChunkReader<S, T>
 where
+    T: Borrow<AudioChunk<S>>,
     S: Copy,
 {
     type Err = std::convert::Infallible;
     fn number_of_channels(&self) -> usize {
-        self.buffer.channels().len()
+        self.chunk.borrow().channels().len()
     }
     fn frames_per_second(&self) -> u64 {
         self.frames_per_second
@@ -49,11 +86,12 @@ where
         assert_eq!(output.number_of_channels(), self.number_of_channels());
         // Note: `self.number_of_channels() > 0`
         let buffer_size = output.number_of_frames();
-        let remainder = self.buffer.channels()[0].len() - self.frame;
+        let remainder = self.chunk.borrow().channels()[0].len() - self.frame;
         let frames_to_copy = std::cmp::min(buffer_size, remainder);
 
-        for (output_channel, input_channel) in
-            output.channel_iter_mut().zip(self.buffer.channels().iter())
+        for (output_channel, input_channel) in output
+            .channel_iter_mut()
+            .zip(self.chunk.borrow().channels().iter())
         {
             assert_eq!(buffer_size, output_channel.len());
             output_channel[0..frames_to_copy]
@@ -61,6 +99,45 @@ where
         }
         self.frame += frames_to_copy;
         Ok(frames_to_copy)
+    }
+}
+
+/// An [`AudioReader`] that reads from a given [`AudioChunk`].
+/// The generic parameter type `S` represents the sample type.
+///
+/// [`AudioReader`]: ../trait.AudioReader.html
+/// [`AudioChunk`]: ../../../buffer/struct.AudioChunk.html
+pub type AudioBufferReader<'b, S> = AudioChunkReader<S, &'b AudioChunk<S>>;
+
+#[cfg(feature = "backend-combined-wav")]
+impl<S> From<(Header, BitDepth)> for AudioChunkReader<S, AudioChunk<S>>
+where
+    S: Copy + FromSample<u8> + FromSample<i16> + FromSample<I24>,
+{
+    fn from((header, samples): (Header, BitDepth)) -> Self {
+        let chunk = match samples {
+            BitDepth::Eight(s) => AudioChunk::from_interlaced_iterator(
+                s.iter().map(|a| S::from_sample_(*a)),
+                header.channel_count as usize,
+            ),
+            BitDepth::Sixteen(s) => AudioChunk::from_interlaced_iterator(
+                s.iter().map(|a| S::from_sample_(*a)),
+                header.channel_count as usize,
+            ),
+            BitDepth::TwentyFour(s) => AudioChunk::from_interlaced_iterator(
+                s.iter().map(|a| {
+                    S::from_sample_(I24::new(*a).expect("24 bits sample should be 24 bits"))
+                }),
+                header.channel_count as usize,
+            ),
+            BitDepth::Empty => AudioChunk::new(header.channel_count as usize),
+        };
+        Self {
+            frames_per_second: header.sampling_rate as u64,
+            frame: 0,
+            chunk,
+            phantom: PhantomData,
+        }
     }
 }
 
