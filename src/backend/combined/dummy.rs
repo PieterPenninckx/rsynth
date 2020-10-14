02beb089a0740d521d271f288bc2d23b79582c9e
Pieter Penninckx <pieter.penninckx@scarlet.be>
2020-10-14 14:15:48 +0200

    More code cleanup.

--- src/backend/combined/dummy.rs
+++ src/backend/combined/dummy.rs
@@ -23,10 +23,7 @@ impl<S> AudioDummy<S> {
         }
     }
 
-    #[deprecated(
-        since = "0.1.2",
-        note = "Deprecated in favour of the dedicated `polyphony` crate."
-    )]
+    #[deprecated(since = "0.1.2", note = "Use `with_sample_rate_and_length` instead.")]
     /// Create a new `AudioDummy` with the "default" sample rate
     /// of 44100 frames per second (CD quality) and a length of 0 samples.
     pub fn new() -> Self {
